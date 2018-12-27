a86940cedb8341c0122f29743e1023aa53e527e0
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-12-26 20:37:12 -0500

    Basic react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
 
-export default App;
+const AppRouter = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/about" component={About} />
+    </div>
+  </Router>
+)
+
+export default AppRouter;
