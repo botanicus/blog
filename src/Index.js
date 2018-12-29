9a82d37bf6599c61e90c9459f63a43b4d6a2a71b
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-12-28 19:23:12 -0500

    Posts listing

--- src/Index.js
+++ src/Index.js
@@ -1,3 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => <h2>Posts</h2>;
+const posts = [
+  {
+    title: "Hello world",
+    excerpt: "Lorem ipsum.",
+    body: "Lorem ipsum."
+  }
+]
+
+class Post extends Component {
+  render() {
+    const { post } = this.props;
+
+    return (
+      <article>
+        <h1>{post.title}</h1>
+        <p>{post.excerpt}</p>
+      </article>
+    );
+  }
+}
+
+export default () => <div>
+    {posts.map((post) => <Post post={post} />)}
+  </div>;
