70bd82ed34d1992e2d829cf095d454b3783b4e7f
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2018-12-28 19:06:23 -0500

    Refactoring and active link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import './App.css';
 
+import About from './About';
+
 const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
 
 const AppRouter = () => (
   <Router>
     <div>
       <nav>
+        <h1><NavLink to="/" activeClassName="selected-link">Blog</NavLink></h1>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
         </ul>
       </nav>
+
+      <footer>
+      </footer>
 
       <Route path="/" exact component={Index} />
       <Route path="/about" component={About} />
