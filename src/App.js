01a916defda1c75bcab27b792975a71543514150
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-01-03 13:00:42 -0500

    Handle no match

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, NavLink } from 'react-router-dom';
 import './App.css';
 
 import About from './About';
-import Index from './Index';
+import Home from './Home';
+import NoMatch from './Errors';
 
 const AppRouter = () => (
   <Router>
@@ -22,8 +23,11 @@ const AppRouter = () => (
       <footer>
       </footer>
 
-      <Route path="/" exact component={Index} />
-      <Route path="/about" component={About} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route component={NoMatch} />
+      </Switch>
     </div>
   </Router>
 )
---
+++ src/Errors.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <h2>No match for <code>{window.location.pathname}</code></h2>;
