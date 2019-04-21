427295086dddfc12636bbaa049b853f4a574a053
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-04-21 16:03:08 +0000

    Encode email

--- src/config.js
+++ src/config.js
@@ -1,12 +1,10 @@
 const config = {
   isProduction: (process.env.NODE_ENV === 'production'),
-  contactEmail: 'james+blog@botanicus.me'
+  contactEmail: Buffer.from('amFtZXMrYmxvZ0Bib3RhbmljdXMubWU=', 'base64').toString('ascii')
 }
 
-if (process.env.NODE_ENV !== 'production') {
+if (config.isProduction) {
   config.googleAnalyticsTrackingId = 'UA-138694948-1'
 }
 
-console.log(Buffer.from("james+blog@botanicus.me").toString('base64'));
-console.log(Buffer.from("SGVsbG8gV29ybGQ=", 'base64').toString('ascii'))
 export default config
