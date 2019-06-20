16ea4b414672e3da01e124880f5ba22bc78a7313
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-06-20 21:33:52 +0000

    Show lang

--- src/PostPreview/PostPreview.js
+++ src/PostPreview/PostPreview.js
@@ -6,9 +6,11 @@ import styles from './PostPreview.module.css'
 // TODO: Use h1, but put it into some <article or what is being used for this and tweak the styles so it isn't that big.
 // ["title", "tags", "digest", "date", "slug", "excerpt", "path", "links"]
 // {title: "zen", slug: "zen", path: "/tags/zen"}
+const lang = '🇬🇧'
+
 export default ({ slug, title, date, tags, excerpt }) => (
   <Fragment>
-    <h2 className={styles.title}><a href={`/posts/${slug}`}>{title}</a></h2>
+    <h2 className={styles.title}><a href={`/posts/${slug}`}>{title} {lang}</a></h2>
     <PostStatusLine date={date} tags={tags} />
     <p>{excerpt}</p>
   </Fragment>
