7266d3eaf96b2a63c4b7449a5fc356cfda1f1c84
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-06-25 22:21:08 +0000

    Working on embedded tweets

--- src/RightColumn/RightColumn.js
+++ src/RightColumn/RightColumn.js
@@ -3,16 +3,16 @@ import { TwitterTimelineEmbed, TwitterTweetEmbed, TwitterShareButton } from 'rea
 
 // https://twitter.com/botanicus/timelines/1143628612630958080
 export default () => (
-  <TwitterTimelineEmbed
-    sourceType="collection"
-    screenName="botanicus"
-    id="1143628612630958080"
-    noScrollbar={true}
-    noHeader={true}
-    noFooter={true}
-    transparent={true}
-    options={{tweetLimit: 2}}
-  />
+  <div />
+  // <TwitterTimelineEmbed
+  //   sourceType="collection"
+  //   screenName="botanicus"
+  //   id="1143628612630958080"
+  //   noHeader={true}
+  //   noFooter={true}
+  //   transparent={true}
+  //   options={{tweetLimit: 2}}
+  // />
 )
     // <TwitterTweetEmbed tweetId={'933354946111705097'} />
 
@@ -20,3 +20,4 @@ export default () => (
     //   url={'https://facebook.com/saurabhnemade'}
     //   options={{ text: '#reactjs is awesome', via: 'saurabhnemade' }}
     // />
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam varius laoreet malesuada. Vivamus sodales est quis quam consequat scelerisque vel ut tellus. Donec semper gravida varius. Proin tortor ex, egestas pretium dolor at, venenatis varius libero. Nulla imperdiet sem nec placerat condimentum. Duis dapibus lectus lorem, eget gravida sem porttitor id. Vestibulum porta, sapien nec bibendum consectetur, lorem sapien suscipit neque, et rhoncus velit mauris eu magna. Suspendisse iaculis posuere neque vel congue. Aliquam consectetur euismod sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris elementum commodo nibh, eu dapibus magna porta ultrices.
