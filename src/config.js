ba34038ec735df6ef8b435f5ea7be7b4d659ccd7
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-06-25 03:07:56 +0000

    Loggly token

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ export const contactEmail = Buffer.from('amFtZXMrYmxvZ0Bib3RhbmljdXMubWU=', 'bas
 export const gravatarEmail = Buffer.from('amFtZXNAYm90YW5pY3VzLm1l', 'base64').toString('ascii')
 export const disqusShortname = 'botanicus-me-1'
 export const mailchimpURL = 'https://botanicus.us3.list-manage.com/subscribe/post?u=3ab289fe1aba79ac1725973d1&amp;id=524bfa9e85'
+export const logglyToken = '8d913c26-cceb-41c3-a82e-e4542ec98ab0'
 export const startYear = 2008
 export const rate = 90
 
