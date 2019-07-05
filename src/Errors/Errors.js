a92cd68d297aa46ce4f773ccb47ffded741525b7
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-07-05 00:28:19 +0000

    Offline detection

--- src/Errors/Errors.js
+++ src/Errors/Errors.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Online, Offline } from 'react-detect-offline'
 import styles from './Errors.module.css'
 
 /* TODO: This should be an error boundary. */
 export const FetchError = (error) => (
   <div className={styles.wrapper}>
     <h1>Data cannot be fetched</h1>
-    <a href={window.location.pathname}>Refresh the page</a>
+    <Online>
+      <a href={window.location.pathname}>Refresh the page</a>
+    </Online>
+
+    <Offline>
+      <div className={styles.offline}>
+        You are currently offline.
+      </div>
+    </Offline>
   </div>
 )
 
