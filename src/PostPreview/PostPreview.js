0db9d22574fcfed84537f24cd828ef10438e8dc7
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-07-06 14:31:46 +0000

    Do not hard-code routes

--- src/PostPreview/PostPreview.js
+++ src/PostPreview/PostPreview.js
@@ -1,12 +1,13 @@
 import React, { memo, Fragment } from 'react'
 import PostStatusLine from '../PostStatusLine/PostStatusLine'
+import { getPostPagePath } from '../routes'
 import styles from './PostPreview.module.css'
 
 const lang = '🇬🇧'
 
 export default memo(({ slug, title, date, tags, excerpt }) => (
   <Fragment>
-    <h2 className={styles.title}><a href={`/posts/${slug}`}>{title} {lang}</a></h2>
+    <h2 className={styles.title}><a href={getPostPagePath(slug)}>{title} {lang}</a></h2>
     <PostStatusLine date={date} tags={tags} />
     <p>{excerpt}</p>
   </Fragment>
--- src/TagList/TagList.js
+++ src/TagList/TagList.js
@@ -1,11 +1,12 @@
 import React, { memo } from 'react'
 import Join from '../Join/Join'
+import { getTagPagePath } from '../routes'
 
 /* \u00a0 is the same as &nbsp; */
 export default memo(({ tags }) => (
   <Join items={tags}>
     {({ slug, name }) => (
-      <a href={`/tags/${slug}`}>{name.replace(/ /g, "\u00a0")}</a>
+      <a href={getTagPagePath(slug)}>{name.replace(/ /g, "\u00a0")}</a>
     )}
   </Join>
 ))
