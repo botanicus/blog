f9f7517b43e39dfc7b25c8fc4ebd03081605d0ab
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2019-07-08 11:32:20 +0000

    Run on CI

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,24 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true)
+describe('Visiting the home page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Shows the header', () => {
+    cy.get('header').
+      should('contain', 'James C Russell')
+    // cy.get('header').should('contain', 'On programming, Ruby, React.js, languages and life.')
+  })
+
+  it('Shows the footer', () => {
+    cy.get('footer').
+      should('contain', 'Posts')
+  })
+
+  it('Does not redirect', () => {
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/')
+      expect(location.hash).to.be.empty
+      expect(location.search).to.be.empty
+    })
   })
 })
