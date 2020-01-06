941c7db16467a53a8eb5b5ec9de973e29ae15258
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-06 18:54:33 +0000

    No Twitter

--- src/SubscribePage/SubscribePage.js
+++ src/SubscribePage/SubscribePage.js
@@ -1,12 +1,6 @@
 import React, { Fragment } from 'react'
 import NewsletterSignUpForm from '../NewsletterSignUpForm/NewsletterSignUpForm'
 import Email from '../Email/Email'
-import { TwitterFollowButton } from 'react-twitter-embed'
-
-import { registerFont, FontAwesomeIcon } from '../FontAwesome/FontAwesome'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-
-registerFont(faTwitter)
 
 export default () => (
   <Fragment>
@@ -23,12 +17,6 @@ export default () => (
     <p>
       At the moment I don't support this option. If you'd like to have it, please <Email subject="Post notifications by email">let me know</Email>.
     </p>
-
-    <p>
-      I post links to new posts on <a href="https://twitter.com/botanicus" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faTwitter} color="#00aced" /> Twitter</a>.
-    </p>
-
-    <TwitterFollowButton screenName="botanicus" />
 
     <h2>RSS feed</h2>
     <p>
