c954a2c53a5c8873bd9081d148f7c5238db19250
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-07 16:37:06 +0000

    Remove footer icons

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,12 +4,7 @@ import { aboutPagePath, subscribePagePath, postsPagePath } from '../routes'
 import { main as mainFooterClassName } from './Footer.module.css'
 import { assert } from '../utils'
 
-import { registerFont, FontAwesomeIcon } from '../FontAwesome/FontAwesome'
-import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
-
 assert(mainFooterClassName, 'mainFooterClassName is expected to be defined')
-
-registerFont(faTwitter, faGithub)
 
 export default memo(() => (
   <footer className={mainFooterClassName}>
@@ -18,18 +13,6 @@ export default memo(() => (
         <li><Link to={postsPagePath}>Posts</Link></li>
         <li><Link to={aboutPagePath}>About me</Link></li>
         <li><Link to={subscribePagePath}>Subscribe</Link></li>
-
-        <li>
-          <a href="https://twitter.com/botanicus" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faTwitter} color="#00aced" />
-          </a>
-        </li>
-
-        <li>
-          <a href="https://github.com/botanicus" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} color="black" />
-          </a>
-        </li>
       </ul>
     </nav>
   </footer>
