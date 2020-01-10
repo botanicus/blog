335c37bbd676346479ac47e58f14c50f8bd28ee6
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-10 22:38:59 +0000

    Add reload link to the Spinner if loading takes too long. Closes #61

--- src/Spinner/Spinner.js
+++ src/Spinner/Spinner.js
@@ -1,6 +1,8 @@
-import React, { Suspense, memo } from 'react'
+import React, { Suspense, memo, useState, useEffect } from 'react'
 import { registerFont, FontAwesomeIcon } from '../FontAwesome/FontAwesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { assert } from '../utils'
+import styles from './Spinner.module.css'
 
 registerFont(faSpinner)
 
@@ -10,13 +12,29 @@ export const SuspenseSpinner = memo(({ children }) => (
   </Suspense>
 ))
 
-const Spinner = memo(({ title }) => (
-  <div style={{marginTop: 10, marginBottom: 10}}>
-    <FontAwesomeIcon icon={faSpinner} spin={true} />{' '}
-    <span style={{color: 'slategrey'}}>
-      {['Loading', title, '...'].filter(Boolean).join(' ')}
-    </span>
-  </div>
-))
+const ReloadLink = () => (
+  <>
+    <em>Taking too long?</em> <a className={assert(styles.link)} href="">Click to reload</a>.
+  </>
+)
+
+const Spinner = memo(function Spinner ({ title }) {
+  const [ takingTooLong, setTakingTooLong ] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => { setTakingTooLong(true) }, 1000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <div style={{marginTop: 10, marginBottom: 10}}>
+      <FontAwesomeIcon icon={faSpinner} spin={true} />{' '}
+      <span style={{color: 'slategrey'}}>
+        {['Loading', title, '...'].filter(Boolean).join(' ')}
+        {takingTooLong && <div style={{marginTop: 20}}><ReloadLink /></div>}
+      </span>
+    </div>
+  )
+})
 
 export default Spinner
