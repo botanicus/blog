f5991276c6ce21e760b86b3eef36f02ea14c1dc1
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-10 22:12:45 +0000

    Gravatar email

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const isProduction = (process.env.NODE_ENV === 'production')
 export const contactEmail = Buffer.from('amFrdWIuc3Rhc3RueS5wdCtibG9nQGdtYWlsLmNvbQ==', 'base64').toString('ascii')
 export const personalContactEmail = Buffer.from('amFrdWIuc3Rhc3RueS5wdEBnbWFpbC5jb20=', 'base64').toString('ascii')
-export const gravatarEmail = Buffer.from('amFtZXNAYm90YW5pY3VzLm1l', 'base64').toString('ascii')
+export const gravatarEmail = Buffer.from('amFrdWIuc3Rhc3RueS5wdCtibG9nQGdtYWlsLmNvbQ==', 'base64').toString('ascii')
 export const mailchimpURL = 'https://botanicus.us3.list-manage.com/subscribe/post?u=3ab289fe1aba79ac1725973d1&amp;id=524bfa9e85'
 
 export const googleAnalyticsTrackingId = 'UA-138694948-1'
