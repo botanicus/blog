bcba97f02c1897f6d4c9aa32ac88bb77c0773dfe
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-13 21:56:21 +0000

    TagPage translated

--- src/TagPage/TagPage.js
+++ src/TagPage/TagPage.js
@@ -1,45 +1,56 @@
 import React, { useEffect, useContext }  from 'react'
 import { useTitle, A } from 'hookrouter'
 import StateContext from '../StateContext'
+import LangContext from '../LangContext'
 import PublishedDate from '../PublishedDate/PublishedDate'
 import Spinner from '../Spinner/Spinner'
 import { getPostPagePath } from '../routes'
 import { assert } from '../utils'
 import styles from './TagPage.module.css'
 
-const PostPreview = ({ title, slug, date }) => (
-  <li>
-    <A href={getPostPagePath(slug)}>{title}</A>{' '}
-    <span className={assert(styles.date)}>
-      <PublishedDate date={date} />
-    </span>
-  </li>
-)
-
-// There are always some posts in the loaded state, otherwise the JSON tag file would never get compiled.
-const PostPreviewList = ({ posts = [] }) => (
-  <ul>
-    {posts.map((post) => <PostPreview key={post.slug} {...post} />)}
-  </ul>
-)
-
-const TagList = ({ name, posts }) => (
-  <>
-    <h1>Posts tagged <span className={assert(styles.emphasis)}>{name}</span></h1>
-    <PostPreviewList posts={posts} />
-  </>
-)
+const translations = {
+  title: {
+    loading: ["Loading ...", "Cargando ..."],
+    loaded: ["Tag", "Loading ..."],
+  },
+  spinner: ["the tag data", "los datos de la etiqueta"],
+  heading: ["Posts tagged", "Entradas etiquetadas"],
+}
 
 export default function Tag ({ slug }) {
   const state = useContext(StateContext)
+  const { t } = useContext(LangContext)
   const tag = state.helpers.getTag(slug)
 
-  useTitle(tag ? `Tag ${tag.name}` : "Loading ...")
+  useTitle(tag ? t(translations.title.loaded) : t(translations.title.loading))
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => { state.helpers.fetchTag(slug) }, [])
 
+  const PostPreview = ({ title, slug, date }) => (
+    <li>
+      <A href={getPostPagePath(slug)}>{title}</A>{' '}
+      <span className={assert(styles.date)}>
+        <PublishedDate date={date} />
+      </span>
+    </li>
+  )
+
+  // There are always some posts in the loaded state, otherwise the JSON tag file would never get compiled.
+  const PostPreviewList = ({ posts = [] }) => (
+    <ul>
+      {posts.map((post) => <PostPreview key={post.slug} {...post} />)}
+    </ul>
+  )
+
+  const TagList = ({ name, posts }) => (
+    <>
+      <h1>{t(translations.heading)} <span className={assert(styles.emphasis)}>{name}</span></h1>
+      <PostPreviewList posts={posts} />
+    </>
+  )
+
   return (
-    tag ? <TagList {...tag} /> : <Spinner title="the tag data" />
+    tag ? <TagList {...tag} /> : <Spinner title={t(translations.spinner)} />
   )
 }
