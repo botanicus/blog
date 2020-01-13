5ac58bef19024be72008292132a6e0a746d06040
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-13 22:11:38 +0000

    Fix tests

--- src/Spinner/Spinner.test.js
+++ src/Spinner/Spinner.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import TestRenderer from 'react-test-renderer'
+import { LangContextProvider } from '../LangContext'
 import Spinner from './Spinner'
 
 it('renders a spinner with a text saying it is loading', () => {
-  const testRenderer = TestRenderer.create(<Spinner>Lorem ipsum</Spinner>)
+  const testRenderer = TestRenderer.create(<LangContextProvider><Spinner>Lorem ipsum</Spinner></LangContextProvider>)
   const div = testRenderer.toJSON()
   expect(div.type).toEqual('div')
 
