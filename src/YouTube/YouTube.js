59ad47afda5a21136d472b5a759f9dbedfc81a81
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-13 21:02:15 +0000

    Fix YouTube URLs

--- src/YouTube/YouTube.js
+++ src/YouTube/YouTube.js
@@ -3,7 +3,7 @@ import { assert } from '../utils'
 
 export default memo(function YouTube ({ src }) {
   return (
-    <iframe title={src} width="560" height="315" src={assert(src).replace(/\bwatch\b/, 'embed')} frameBorder="0"
+    <iframe title={src} width="560" height="315" src={assert(src).replace(/\bwatch\?v=(.+)$/, 'embed/$1')} frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
     </iframe>
   )
