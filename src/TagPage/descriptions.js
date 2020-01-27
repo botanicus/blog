3a469d287d18069292995a38bd9142b077224f35
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-27 17:57:38 +0000

    More descriptions

--- src/TagPage/descriptions.js
+++ src/TagPage/descriptions.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import OutboundLink from '../OutboundLink/OutboundLink'
 
+const Derek = ({ children }) => (
+  <OutboundLink eventLabel="derek-sivers" to="https://sivers.org/nowff">
+    {children}
+  </OutboundLink>
+)
+
 export default {
   en: {
     now: <>
-      The now page is <OutboundLink eventLabel="derek-sivers" to="https://sivers.org/nowff">Derek Siver's idea</OutboundLink> to respond the question <em>"what have you been up to lately?"</em>. This is archive of all my past updates.
+      The now page is <Derek>Derek Siver's idea</Derek> to respond the question <em>"what have you been up to lately?"</em>.
+      This is archive of all my past updates.
     </>,
     diving: <>
       Diving is one of my favourite activities, ever since I started in January 2018.
@@ -14,12 +21,18 @@ export default {
     </>,
     God: <>
       My idea of God is more modern, more in the notion of <em>Light</em>, <em>Universe</em>, not as an wise old man sitting somewhere in the clouds.
+    </>,
+    healing: <>
+      During my spiritual awakening, I found out (to my great surprise) that I can do healings with hands or remotely.
+      While I like doing so, I'm not an expert on the subject and I'm still unclear for instance about when energy healing
+      works and when doesn't.
     </>
   },
 
   es: {
     ahora: <>
-      The now page is <OutboundLink eventLabel="derek-sivers" to="https://sivers.org/nowff">Derek Siver's idea</OutboundLink> to respond the question <em>"what have you been up to lately?"</em>. This is archive of all my past updates.
+      The now page is <Derek>Derek Siver's idea</Derek> to respond the question <em>"what have you been up to lately?"</em>.
+      This is archive of all my past updates.
     </>
   }
 }
