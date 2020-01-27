40bea4103ced2fdf40af711ba361920d4fffaa98
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-27 19:59:09 +0000

    Fix moment lang. Closes #85

--- src/PublishedDate/PublishedDate.js
+++ src/PublishedDate/PublishedDate.js
@@ -1,9 +1,14 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
+import LangContext from '../LangContext'
 import Moment from 'react-moment'
 
 /* Update every 30 seconds. */
 const updateInterval = 30 * 1000
 
-export default memo(({ date }) => (
-  <Moment date={new Date(date)} fromNow interval={updateInterval} />
-))
+export default memo(function PublishedDate ({ date }) {
+  const { lang } = useContext(LangContext)
+
+  return (
+    <Moment date={new Date(date)} fromNow interval={updateInterval} locale={lang} />
+  )
+})
