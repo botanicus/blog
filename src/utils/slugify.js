41f4f824b3ba8be25d0dfa6cdbc0146d837159d6
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-27 15:35:54 +0000

    Transliteration doesn't work

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,3 +1,11 @@
-import transliteration from 'transliteration'
+// FIXME: transliteration doesn't seem to export anything,
+// even though it works on the backend.
+//
+// import { transliterate, slugify } from 'transliteration'
+// import * as transliterate from 'transliteration'
 
-export default transliteration.slugify
+// export default slugify
+
+export default function slugify (name) {
+  return name.toLocaleLowerCase().replace(' ', '-')
+}
