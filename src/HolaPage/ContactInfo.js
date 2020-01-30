e6dc64fcf156ac95e1ebe61a73a8627ffa20f45e
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-30 21:14:25 +0000

    Add Google Hangouts

--- src/HolaPage/ContactInfo.js
+++ src/HolaPage/ContactInfo.js
@@ -7,5 +7,6 @@ export default () => (
     <li>Telegram <OutboundLink eventLabel="telegram" to="https://t.me/jakubstastny">@jakubstastny</OutboundLink></li>
     <li>Email <Email personal /></li>
     <li>iMessage <Email personal /></li>
+    <li>Google Hangouts <Email personal /></li>
   </ul>
 )
