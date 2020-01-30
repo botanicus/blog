10522b09ce5fcdb9dac0b36b97493e42cde4251a
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-01-30 21:32:23 +0000

    Google Hangouts will be killed

--- src/HolaPage/ContactInfo.js
+++ src/HolaPage/ContactInfo.js
@@ -7,6 +7,6 @@ export default () => (
     <li>Telegram <OutboundLink eventLabel="telegram" to="https://t.me/jakubstastny">@jakubstastny</OutboundLink></li>
     <li>Email <Email personal /></li>
     <li>iMessage <Email personal /></li>
-    <li>Google Hangouts <Email personal /></li>
+    {/* Don't bother with Google Hangouts, Google will kill it in June 2020 and will replace it will Google Hangouts Chat, which is only for Google Apps for business and with Google Meet which is only for meetings. */}
   </ul>
 )
