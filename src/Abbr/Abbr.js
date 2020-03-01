9c720cd86a2e22ee71b4cb6fccecbd919c5c0c77
James C Russell <jakub.stastny.pt+botanicus@gmail.com>
2020-03-01 01:21:23 +0000

    Remove obsolete code

--- src/Abbr/Abbr.js
+++ src/Abbr/Abbr.js
@@ -1,10 +1,6 @@
 /* TODO: import the tooltip code from src/PostPage/PostPage.js */
 
 import React, { memo } from 'react'
-// import { hashtag as hashtagClassName } from './HashTag.module.css'
-// import { assert } from '../utils'
-
-// assert(hashtagClassName, 'hashtagClassName is expected to be defined')
 
 export default memo(({ title, children }) => (
   <abbr title={title}>{children}</abbr>
